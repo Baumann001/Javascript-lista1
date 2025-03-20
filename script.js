5814ecf055d7e22b65681dee42f724954fd12295
Baumann001 <gustavo_baumann@estudante.sesisenai.org.br>
2025-03-20 09:05:58 -0300

    add-

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-console.log('helo world')
+console.log('helo world');
+let= numero prompt ('digite um número:');
+function verificarPar(numero) {
+    if(numero % 2 === 0){
+        console.log(numero + " é igual a par.");
+    }else{
+        console.log(numero+ "é igual a impar.");
+    }
 
-function verificaPar(numero) {
-    console.log(numero % 2 ===0? "par" : "impar");
-     
+
 }
 
