05f040fdf2e910d26159f8a44bbd0d41927fb61c
Baumann001 <gustavo_baumann@estudante.sesisenai.org.br>
2025-03-20 09:36:13 -0300

    add-

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
+/*
+Atividade 1 =
+
 console.log('helo world');
+
 let numero= prompt ('digite um número:');
 function verificarPar(numero) {
     if(numero % 2 === 0){
         console.log(numero + " é igual a par.");
     }else{
-        console.log(numero+ "é igual a impar.");
+        console.log(numero + "é igual a impar.");
     }
 
 
 }
+    verificarPar(numero);
 
+*/
+
+// variáveis:
+let numero= prompt('Digite um número');
+let n1 = parsefloat(prompt ('digite um numero:'));
+let n2= parsefloat(prompt ('Digite outro numero'));
+ 
+function calcular(n1,n2){
+console.log(n1+n2);
+console.log(n1-n2);
+console.log(n1*n2);
+ if (n2 ==0)
+
+
+
+
+ 
+ 
+ 
