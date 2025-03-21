2074a81709eae99de7acb3331ab6451336ea033a
Baumann001 <gustavo_baumann@estudante.sesisenai.org.br>
2025-03-21 09:42:04 -0300

    add- comnetários novo

--- script.js
+++ script.js
@@ -126,13 +126,14 @@ for( let i=1;i<=20; i++){
 }
 */
 
+/*
 function  verificaParli(palavra){
     let invertida = palavra.split('').reverse().join('');
     return palavra === invertida;
     
 }
 
-/*
+
 Atividade 10 =
 let palavra = prompt("Digite uma palavra para identificar se é um palíndromo:");
 console.log(`A palavra ${palavra}" é um palíndromo? ${verificaParli(palavra)}`);
