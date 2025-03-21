c27363942127e4448cfa2b15b4e74af5bfcc8a4f
Baumann001 <gustavo_baumann@estudante.sesisenai.org.br>
2025-03-21 09:27:34 -0300

    add- para

--- script.js
+++ script.js
@@ -115,6 +115,15 @@ let n3=parseFloat(prompt("Digite o terceiro número:"));
 media(n1,n2,n3);
 */
 
+for( let i=1;i<=20; i++){
+    if (i % 3 === 0) { //se o resto da divisao de 3 resta 0,
+        console.log( i + " é múltiplo de 3");
+    }else{
+        console.log( i+ "não é multiplo de 3");
+    }
+}
+
+
 
 
 
